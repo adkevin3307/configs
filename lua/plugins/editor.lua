22593c4efd458a50701214b9360ad5ffa4fd5ef8
adkevin3307 <adkevin3307@gmail.com>
2023-10-30 02:24:26 +0800

    update GitSignes configuration

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -119,12 +119,45 @@ return {
         "lewis6991/gitsigns.nvim",
         config = function()
             require("gitsigns").setup({
-                current_line_blame = true
+                signs = {
+                    add = { hl = "GitSignsAdd", text = "▎", numhl = "GitSignsAddNr", linehl = "GitSignsAddLn" },
+                    change = { hl = "GitSignsChange", text = "▎", numhl = "GitSignsChangeNr", linehl = "GitSignsChangeLn" },
+                    delete = { hl = "GitSignsDelete", text = "契", numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn" },
+                    topdelete = { hl = "GitSignsDelete", text = "契", numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn" },
+                    changedelete = { hl = "GitSignsChange", text = "▎", numhl = "GitSignsChangeNr", linehl = "GitSignsChangeLn" }
+                },
+                signcolumn = true,
+                numhl = false,
+                linehl = false,
+                word_diff = false,
+                watch_gitdir = {
+                    follow_files = true
+                },
+                attach_to_untracked = true,
+                current_line_blame = true,
+                current_line_blame_opts = {
+                    virt_text = true,
+                    virt_text_pos = "eol",
+                    delay = 3000,
+                    ignore_whitespace = false,
+                    virt_text_priority = 100
+                },
+                current_line_blame_formatter = "<author>, <author_time:%Y-%m-%d> - <summary>",
+                sign_priority = 6,
+                update_debounce = 100,
+                status_formatter = nil,
+                max_file_length = 40000,
+                preview_config = {
+                    border = "single",
+                    style = "minimal",
+                    relative = "cursor",
+                    row = 0,
+                    col = 1
+                },
+                yadm = {
+                    enable = false
+                }
             })
-
-            local colors = require("onedark.colors")
-            vim.api.nvim_set_hl(0, "GitSignsAddPreview", { bg = colors.none, fg = colors.green })
-            vim.api.nvim_set_hl(0, "GitSignsDeletePreview", { bg = colors.none, fg = colors.red })
 
             local keymap = vim.keymap.set
             keymap("n", "<Leader>gn", ":Gitsigns next_hunk<CR>", { silent = true })
