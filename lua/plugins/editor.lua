230ce9f55b3931b6561b666df99ad08ce776ad18
adkevin3307 <adkevin3307@gmail.com>
2023-10-30 16:47:27 +0800

    remove custom sign for GitSigns

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -119,13 +119,6 @@ return {
         "lewis6991/gitsigns.nvim",
         config = function()
             require("gitsigns").setup({
-                signs = {
-                    add = { hl = "GitSignsAdd", text = "▎", numhl = "GitSignsAddNr", linehl = "GitSignsAddLn" },
-                    change = { hl = "GitSignsChange", text = "▎", numhl = "GitSignsChangeNr", linehl = "GitSignsChangeLn" },
-                    delete = { hl = "GitSignsDelete", text = "契", numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn" },
-                    topdelete = { hl = "GitSignsDelete", text = "契", numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn" },
-                    changedelete = { hl = "GitSignsChange", text = "▎", numhl = "GitSignsChangeNr", linehl = "GitSignsChangeLn" }
-                },
                 signcolumn = true,
                 numhl = false,
                 linehl = false,
