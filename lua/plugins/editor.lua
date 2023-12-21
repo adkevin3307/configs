e8075c56a72543270f4cefbc6e2ae95d49d02736
adkevin3307 <adkevin3307@gmail.com>
2023-12-21 17:48:11 +0800

    update barbecue.nvim

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -127,8 +127,6 @@ return {
     },
     {
         "utilyre/barbecue.nvim",
-        name = "barbecue",
-        version = "*",
         dependencies = {
             "SmiteshP/nvim-navic",
             "nvim-tree/nvim-web-devicons"
