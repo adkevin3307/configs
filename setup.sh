2e69dc1161cfed708649300f69bc23689bc17e83
adkevin3307 <adkevin3307@gmail.com>
2024-06-07 12:37:24 +0800

    fix nvim version to v0.10.0

--- setup.sh
+++ setup.sh
@@ -118,7 +118,7 @@ function install_editor()
         cp -vf $PWD/coc-settings.json ~/.vim
         cp -vf $PWD/.vimrc ~
     elif [[ $EDITOR == "NVIM" ]]; then
-        curl -LO https://github.com/neovim/neovim/releases/download/nightly/nvim.appimage
+        curl -LO https://github.com/neovim/neovim/releases/download/v0.10.0/nvim.appimage
         chmod u+x nvim.appimage
         ./nvim.appimage --appimage-extract
         execute 'mv squashfs-root /'
