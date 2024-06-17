64b8a1e96982170f57beda10dbd019ec3a4c4235
adkevin3307 <adkevin3307@gmail.com>
2024-06-17 16:27:56 +0800

    remove event for lspconfig

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -7,10 +7,6 @@ return {
     },
     {
         "neovim/nvim-lspconfig",
-        event = {
-            "BufReadPre",
-            "BufNewFile"
-        },
         dependencies = {
             {
                 "williamboman/mason-lspconfig.nvim",
