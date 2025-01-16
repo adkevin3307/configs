050007e4da0657a613ec18cc89263d3c8ebab9a5
adkevin3307 <adkevin3307@gmail.com>
2025-01-16 23:14:49 +0800

    remove search completion

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -77,14 +77,6 @@ return {
                                     { name = "path" }
                                 }
                             })
-
-                            -- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
-                            cmp.setup.cmdline({ '/' }, {
-                                mapping = cmp.mapping.preset.cmdline(),
-                                sources = {
-                                    { name = 'buffer' }
-                                }
-                            })
                         end
                     }
                 },
