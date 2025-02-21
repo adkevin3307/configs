4c7c90a4b23de92c5ece7e7b2d06afa781e5d926
adkevin3307 <adkevin3307@gmail.com>
2025-02-21 14:25:54 +0800

    replace dressing.nvim to snacks.nvim

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -1,10 +1,9 @@
 return {
     {
-        "stevearc/dressing.nvim",
+        "folke/snacks.nvim",
         opts = {
-            input = {
-                enabled = false,
-            },
+            input = { enabled = true },
+            picker = { enabled = true },
         },
     },
     {
