3dd6c106d224d6a8b4871bd693a67b53cc180646
adkevin3307 <adkevin3307@gmail.com>
2025-06-05 11:28:41 +0800

    remove locale setting in setup.sh

--- setup.sh
+++ setup.sh
@@ -100,7 +100,6 @@ function install_packages()
             ;;
         arch)
             execute 'pacman -S zsh gcc git bat fzf btop tmux tree curl diffutils unzip man-db fd ripgrep python-pip python-virtualenv nodejs npm --noconfirm'
-            execute 'localectl set-locale LANG=en_US.UTF-8'
             execute 'mandb'
 
             ;;
