c2868c4dae4f0203b0ece5dcd1c35f834cac4517
adkevin3307 <adkevin3307@gmail.com>
2025-10-27 14:45:00 +0800

    set lualine without bold

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -85,10 +85,45 @@ return {
         "nvim-lualine/lualine.nvim",
         lazy = false,
         priority = 1000,
-        opts = {
-            options = {
-                theme = "onedark",
-            },
+        dependencies = {
+            "nvim-tree/nvim-web-devicons",
+            "navarasu/onedark.nvim",
         },
+        config = function()
+            local c = require("onedark.colors")
+            local cfg = vim.g.onedark_config
+
+            local colors = {
+                bg = c.bg0,
+                fg = c.fg,
+                red = c.red,
+                green = c.green,
+                yellow = c.yellow,
+                blue = c.blue,
+                purple = c.purple,
+                cyan = c.cyan,
+                gray = c.grey,
+            }
+
+            local onedark = {
+                inactive = {
+                    a = { fg = colors.gray, bg = colors.bg },
+                    b = { fg = colors.gray, bg = colors.bg },
+                    c = { fg = colors.gray, bg = cfg.lualine.transparent and c.none or c.bg1 },
+                },
+                normal = {
+                    a = { fg = colors.bg, bg = colors.green },
+                    b = { fg = colors.fg, bg = c.bg3 },
+                    c = { fg = colors.fg, bg = cfg.lualine.transparent and c.none or c.bg1 },
+                },
+                visual = { a = { fg = colors.bg, bg = colors.purple } },
+                replace = { a = { fg = colors.bg, bg = colors.red } },
+                insert = { a = { fg = colors.bg, bg = colors.blue } },
+                command = { a = { fg = colors.bg, bg = colors.yellow } },
+                terminal = { a = { fg = colors.bg, bg = colors.cyan } },
+            }
+
+            require("lualine").setup({ options = { theme = onedark } })
+        end,
     },
 }
