00f5d04d5966540bcceca8e0805bb5d8c58f0daf
adkevin3307 <adkevin3307@gmail.com>
2026-06-18 12:58:38 +0800

    change agent

--- lua/plugins/agent.lua
+++ lua/plugins/agent.lua
@@ -1,6 +1,7 @@
 return {
     {
         "yetone/avante.nvim",
+        enabled = false,
         build = vim.fn.has("win32") ~= 0 and "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" or "make",
         event = "VeryLazy",
         version = false,
@@ -33,7 +34,7 @@ return {
                 ollama = {
                     ["local"] = true,
                     __inherited_from = "openai",
-                    endpoint = "http://localhost:11434/v1",
+                    endpoint = "",
                     model = "qwen3-coder:30b",
                     disable_tools = false,
                     api_key_name = "",
@@ -45,7 +46,7 @@ return {
                     },
                 },
                 openai = {
-                    endpoint = "http://localhost/v1",
+                    endpoint = "",
                     model = "NVIDIA-Nemotron-3-Ultra-550B-A55B",
                     disable_tools = false,
                     extra_request_body = {
@@ -53,6 +54,144 @@ return {
                     },
                 },
             },
+        },
+    },
+    {
+        "olimorris/codecompanion.nvim",
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            "nvim-treesitter/nvim-treesitter",
+
+            "ravitemer/mcphub.nvim",
+            {
+                "MeanderingProgrammer/render-markdown.nvim",
+                ft = { "markdown", "codecompanion" },
+            },
+            {
+                "HakonHarnes/img-clip.nvim",
+                opts = {
+                    filetypes = {
+                        codecompanion = {
+                            prompt_for_file_name = false,
+                            template = "[Image]($FILE_PATH)",
+                            use_absolute_path = true,
+                        },
+                    },
+                },
+            },
+        },
+        config = function()
+            require("codecompanion").setup({
+                opts = {
+                    log_level = "DEBUG",
+                },
+                interactions = {
+                    chat = {
+                        adapter = "ollama",
+                        model = "qwen3-coder:30b",
+                        keymaps = {
+                            send = {
+                                modes = {
+                                    n = "<C-s>",
+                                    i = "<C-s>",
+                                },
+                            },
+                            close = {
+                                modes = {
+                                    n = "<C-c>",
+                                    i = "<C-c>",
+                                },
+                            },
+                        },
+                    },
+                    inline = {
+                        keymaps = {
+                            stop = {
+                                modes = {
+                                    n = "<C-c>",
+                                    i = "<C-c>",
+                                },
+                            },
+                        },
+                    },
+                    shared = {
+                        keymaps = {
+                            always_accept = {
+                                modes = {
+                                    n = "Y",
+                                },
+                            },
+                            accept_change = {
+                                modes = {
+                                    n = "y",
+                                },
+                            },
+                            reject_change = {
+                                modes = {
+                                    n = "n",
+                                },
+                            },
+                            next_hunk = {
+                                modes = {
+                                    n = "<Tab>",
+                                },
+                            },
+                            previous_hunk = {
+                                modes = {
+                                    n = "<S-Tab>",
+                                },
+                            },
+                        },
+                    },
+                },
+                display = {
+                    action_palette = {
+                        provider = "telescope",
+                    },
+                    chat = {
+                        window = {
+                            layout = "vertical",
+                            position = "right",
+                            width = 0.3,
+                        },
+                        show_header_separator = true,
+                        fold_context = true,
+                    },
+                },
+                adapters = {
+                    http = {
+                        ollama = function()
+                            return require("codecompanion.adapters").extend("ollama", {
+                                env = {
+                                    url = "",
+                                },
+                            })
+                        end,
+                        openai = function()
+                            return require("codecompanion.adapters").extend("openai", {
+                                env = {
+                                    url = "",
+                                    api_key = "",
+                                },
+                            })
+                        end,
+                    },
+                },
+                extensions = {
+                    mcphub = {
+                        callback = "mcphub.extensions.codecompanion",
+                        opts = {
+                            make_vars = true,
+                            make_slash_commands = true,
+                            show_result_in_chat = true,
+                        },
+                    },
+                },
+            })
+        end,
+        keys = {
+            { "<Leader>aa", "<CMD>CodeCompanionAction<CR>", mode = { "n" }, desc = "Open agent action" },
+            { "<Leader>ac", "<CMD>CodeCompanionChat Toggle<CR>", mode = { "n" }, desc = "Toggle agent chat" },
         },
     },
 }
